54f4549892815a9ce91c4d36fdfba5ef54428863
Maheshgoud6181 <maheshkalal9390@gmail.com>
2025-10-11 21:35:42 +0530

    fouth years added and form link

--- src/TeamData/2026.js
+++ src/TeamData/2026.js
@@ -1,40 +1,127 @@
 // src/teamData/2026.js
+import vikrant from "../images/vikrant.jpg";
+import nirdesh from "../images/nirdesh.jpg";
+import rishab from "../images/rishab.jpg"
+import vijay from "../images/vijay.jpg";
+import sravan from "../images/sravan.jpg";
+import senior4 from "../images/senior4.jpg"
+import senior3 from "../images/senior3.jpg";
+import senior2 from "../images/senior2.jpg";
+import sathi from "../images/sathi.jpg"
 import dummy from "../images/dummy.png"
 
 export const batch2026 = [
   
-    {
-    id: 21,
-    name: "To Be Announced",
-    role: "Incoming Member",
-    image: dummy, // go two steps up if your image is stored there
-    social: {
-      linkedin: "#",
-      email: "pending@university.edu",
-      insta: "#"
-    }
-  },
   {
-    id: 22,
-    name: "To Be Announced",
-    role: "Incoming Member",
-    image: dummy,
-    social: {
-      linkedin: "#",
-      email: "pending@university.edu",
-      insta: "#"
-    }
-  },
-  {
-    id: 23,
-    name: "To Be Announced",
-    role: "Incoming Member",
-    image:dummy,
-    social: {
-      linkedin: "#",
-      email: "pending@university.edu",
-      insta: "#"
-    }
+  id: 21,
+  name: "Vikrant Jaiswal",
+  role: "President",
+  image: vikrant,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
   }
+},
+{
+  id: 22,
+  name: "Sherma Saren",
+  role: "Vice President",
+  image: senior4,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+},
+{
+  id: 23,
+  name: "Sai Sravan",
+  role: "Vice President",
+  image: sravan,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+},
+{
+  id: 24,
+  name: "Nirdesh Regmi",
+  role: "General Secretary",
+  image: nirdesh,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+},
+{
+  id: 25,
+  name: "Shakir Shoaib",
+  role: "Treasurer",
+  image: senior3,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+},
+{
+  id: 26,
+  name: "Sreejita Biswas",
+  role: "R & D Head",
+  image: senior2,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+},
+{
+  id: 27,
+  name: "Sathi Nandi",
+  role: "Tech Head",
+  image: sathi,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+},
+{
+  id: 28,
+  name: "Sreeja Kar",
+  role: "Content Head",
+  image: dummy,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+},
+{
+  id: 29,
+  name: "Harshada Vijay Pawar",
+  role: "P.R. Head",
+  image: vijay,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+},
+{
+  id: 30,
+  name: "Rishabh Sharma",
+  role: "Tech & Logistics Head",
+  image: rishab,
+  social: {
+    linkedin: "#",
+    email: "pending@university.edu",
+    insta: "#"
+  }
+}
+
   
   ];  
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,59 +1,81 @@
-import './Header.css';
+import "./Header.css";
 
-import logoEvolvere from '../../images/logoevolvere1.png';
-import depart from '../../images/depart.png';
+import logoEvolvere from "../../images/logoevolvere1.png";
+import depart from "../../images/depart.png";
 import { useState } from "react";
 
-
-
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <>
-            <nav id="navbar">
-                <div className='logo-container'>
-                    <a href="/" className="logo">
-                        <img src={logoEvolvere} alt="Evolvere Logo" className="logoevolvere" />
-                    </a>
-                    
-                </div>
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <nav id="navbar">
+        <div className="logo-container">
+          <a href="/" className="logo">
+            <img
+              src={logoEvolvere}
+              alt="Evolvere Logo"
+              className="logoevolvere"
+            />
+          </a>
+        </div>
 
-                <button
-                    className="mobile-menu-btn"
-                    onClick={() => setIsOpen(!isOpen)}
+        <button className="mobile-menu-btn" onClick={() => setIsOpen(!isOpen)}>
+          <i className={isOpen ? "fas fa-times" : "fas fa-bars"}></i>
+        </button>
+
+        <div className={`nav-links ${isOpen ? "active" : ""}`}>
+          <a href="#about" onClick={() => setIsOpen(false)}>
+            About
+          </a>
+          <a href="#what-we-do" onClick={() => setIsOpen(false)}>
+            What We Do
+          </a>
+          <a href="#events" onClick={() => setIsOpen(false)}>
+            Events
+          </a>
+          <a href="#gallery" onClick={() => setIsOpen(false)}>
+            Gallery
+          </a>
+          <a href="#team" onClick={() => setIsOpen(false)}>
+            Team
+          </a>
+          <a href="#contact" onClick={() => setIsOpen(false)}>
+            Contact
+          </a>
+        </div>
+      </nav>
+
+      <section className="hero">
+        <div>
+          <div className="container">
+            <div className="hero-content">
+              <h1>Evolvere | Biotech Society of NIT DGP</h1>
+              <p>
+                Join our society to explore biotech in creative, collaborative
+                ways — from managing events and crafting content to designing
+                graphics, developing the web, or curating research . Be part of
+                the vibrant culture on campus.
+              </p>
+              <div className="hero-btns">
+                <a href="#about" class="btn">
+                  Learn More
+                </a>
+                <a
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSdyKCVl6UYct1NFRUTX2UU54sHaqsKcZlLeCW3_-LJAcc3FYg/viewform?usp=sharing&ouid=101496448735176085028"
+                  class="btn btn-outline"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                    <i className={isOpen ? "fas fa-times" : "fas fa-bars"}></i>
-                </button>
+                  Join Us
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
 
-                <div className={`nav-links ${isOpen ? "active" : ""}`}>
-                    <a href="#about" onClick={() => setIsOpen(false)}>About</a>
-                    <a href="#what-we-do" onClick={() => setIsOpen(false)}>What We Do</a>
-                    <a href="#events" onClick={() => setIsOpen(false)}>Events</a>
-                    <a href="#gallery" onClick={() => setIsOpen(false)}>Gallery</a>
-                    <a href="#team" onClick={() => setIsOpen(false)}>Team</a>
-                    <a href="#contact" onClick={() => setIsOpen(false)}>Contact</a>
-                </div>
-            </nav>
-
-            <section className="hero">
-                <div>
-
-
-                    <div className="container">
-                        <div className="hero-content">
-                            <h1>Evolvere | Biotech Society of NIT DGP</h1>
-                            <p>Join our society to explore biotech in creative, collaborative ways — from managing events and crafting content to designing graphics, developing the web, or curating research . Be part of the vibrant culture on campus.</p>
-                            <div className="hero-btns">
-                                <a href="#about" class="btn">Learn More</a>
-                                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfPQHSsEDwCxsVByhGdKvGaJdDXQNxn9Vd1iReQ3F_stw8KTw/viewform?usp=dialog" class="btn btn-outline" target="_blank" rel="noopener noreferrer">Join Us</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <img src={depart} alt="hi" className="department" />
-            </section>
-        </>
-    )
-}
-export default Header
+        <img src={depart} alt="hi" className="department" />
+      </section>
+    </>
+  );
+};
+export default Header;
